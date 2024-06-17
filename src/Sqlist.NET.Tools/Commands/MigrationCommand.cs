4cde86143a9d73f7e4c60c0dac0245a536d09af5
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-17 15:22:16 +0300

    Implemented the migration command and defined the migration handler.

---
+++ src/Sqlist.NET.Tools/Commands/MigrationCommand.cs
@@ -0,0 +1,24 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using Sqlist.NET.Tools.Handlers;
+using Sqlist.NET.Tools.Properties;
+
+namespace Sqlist.NET.Tools.Commands;
+
+/// <summary>
+///     Initializes a new instance of the <see cref="MigrationCommand"/> class.
+/// </summary>
+internal class MigrationCommand(MigrationHandler handler, ICommandInitializer initializer) : CommandBase<MigrationHandler>(handler, initializer)
+{
+    private readonly MigrationHandler _handler = handler;
+
+    public override void Configure(CommandLineApplication command)
+    {
+        if (Configured) return;
+
+        _handler.Project = command.Option("-p|--project <PROJECT>", Resources.ProjectDescription, CommandOptionType.SingleValue);
+        _handler.NoBuild = command.Option("--no-build", Resources.NoBuildDescription, CommandOptionType.NoValue);
+
+        base.Configure(command);
+    }
+}
---
+++ src/Sqlist.NET.Tools/Handlers/MigrationHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Sqlist.NET.Tools.Handlers;
+internal class MigrationHandler : ICommandHandler
+{
+    public CommandOption? Project { get; set; }
+    public CommandOption? NoBuild { get; set; }
+
+    public bool Transmittable => true;
+
+    public Task<int> OnExecuteAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
