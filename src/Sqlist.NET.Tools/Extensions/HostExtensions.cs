d5fdc630ebe6085b2edca9d869ad897745f548f6
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-17 18:27:00 +0300

    Implemented the extension helpers that handle the service registrations for both the handlers and the transmitter.

---
+++ src/Sqlist.NET.Tools/Extensions/HostExtensions.cs
@@ -0,0 +1,27 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+using Sqlist.NET.Tools.Commands;
+using Sqlist.NET.Tools.Services;
+
+namespace Sqlist.NET.Tools.Extensions;
+internal static class HostExtensions
+{
+    public static IHostBuilder UseCommandLineApplication<TCommand>(this IHostBuilder host) where TCommand : ICommand
+    {
+        var app = new CommandLineApplication { Name = "dotnet sqlist" };
+
+        host.ConfigureServices(services =>
+        {
+            services.AddCommonServices();
+            services.TryAddSingleton(app);
+
+            services.AddHostedService<ConsoleService<TCommand>>();
+        });
+
+        return host;
+    }
+}
---
+++ src/Sqlist.NET.Tools/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Sqlist.NET.Infrastructure.Internal;
+using Sqlist.NET.Tools;
+using Sqlist.NET.Tools.Commands;
+using Sqlist.NET.Tools.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static SqlistBuilder AddSqlistTools(this SqlistBuilder builder)
+    {
+        builder.Services.TryAddSingleton<MigrationCommand>();
+        builder.Services.AddHostedService<MigrationService>();
+        builder.Services.AddCommonServices();
+
+        return builder;
+    }
+
+    internal static void AddCommonServices(this IServiceCollection services)
+    {
+        services.TryAddSingleton<ICommandInitializer, CommandInitializer>();
+        services.TryAddSingleton<ICommandTransmitter, CommandTransmitter>();
+        services.TryAddSingleton<IProcessRunner, ProcessRunner>();
+    }
+}
