3b4010fcd58228fbbfc113a9538abe8ae0d025c7
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-17 18:14:25 +0300

    Implemented the migration hosted service, which is the unit that runs the migration within a specific project that's to be targeted by underlying call from the .NET tool.

---
+++ src/Sqlist.NET.Tools/Services/MigrationService.cs
@@ -0,0 +1,30 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using Microsoft.Extensions.Hosting;
+
+using Sqlist.NET.Tools.Commands;
+
+namespace Sqlist.NET.Tools.Services;
+
+/// <summary>
+///     Initializes a new instance of the <see cref="MigrationService"/> class.
+/// </summary>
+internal class MigrationService(MigrationCommand migrationCommand, IHostApplicationLifetime lifetime) : IHostedService
+{
+    static readonly string[] _cmdArgs = Environment.GetCommandLineArgs();
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var app = new CommandLineApplication();
+
+        migrationCommand.Configure(app);
+        migrationCommand.OnCompleted += lifetime.StopApplication;
+
+        await app.ExecuteAsync(_cmdArgs, cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
