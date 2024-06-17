e15663baa710e0c0a212705af8ef929c64c8d883
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-17 18:22:40 +0300

    Implemented the hosted service that represents the entry point for command execution within the context of the foreseen host based console application.

---
+++ src/Sqlist.NET.Tools/Services/ConsoleService.cs
@@ -0,0 +1,47 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Sqlist.NET.Tools.Commands;
+
+namespace Sqlist.NET.Tools.Services;
+
+/// <summary>
+///     Initializes a new instance of the <see cref="ConsoleService"/> class.
+/// </summary>
+internal class ConsoleService<TCommand>(
+    IHostApplicationLifetime lifetime, CommandLineApplication app, TCommand rootCommand, ILogger<ConsoleService<TCommand>> logger) : IHostedService where TCommand : ICommand
+{
+    static readonly string[] _cmdArgs = Environment.GetCommandLineArgs();
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        lifetime.ApplicationStarted.Register(() =>
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    rootCommand.Configure(app);
+                    await app.ExecuteAsync(_cmdArgs, cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Unhandled exception!");
+                }
+                finally
+                {
+                    lifetime.StopApplication();
+                }
+            });
+        });
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
