e5dcbda9e57a6537297592090ace79592dfbad1e
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-17 18:43:47 +0300

    Created the application's entry point utilizing the elements of the .NET tool.

---
+++ src/Sqlist.NET.Tools/Program.cs
@@ -0,0 +1,28 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sqlist.NET.Tools.Commands;
+using Sqlist.NET.Tools.Extensions;
+using Sqlist.NET.Tools.Logging;
+
+try
+{
+    await Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton<RootCommand>();
+            services.AddSingleton<MigrationCommand>();
+        })
+        .UseCommandLineApplication<RootCommand>()
+        .RunConsoleAsync();
+}
+catch (Exception ex)
+{
+    if (ex is CommandParsingException)
+        Reporter.WriteVerbose(ex.ToString());
+    else
+        Reporter.WriteInformation(ex.ToString());
+
+    Reporter.WriteError(ex.Message);
+}
