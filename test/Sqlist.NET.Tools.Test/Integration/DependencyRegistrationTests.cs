08290125aea41b0863a72b04157bc24bfbc85b29
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-23 00:52:47 +0300

    Added new test to verify whether dependencies are correctly registered for either service.

---
+++ test/Sqlist.NET.Tools.Test/Integration/DependencyRegistrationTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Moq;
+
+using Sqlist.NET.Extensions;
+using Sqlist.NET.Tools.Extensions;
+using Sqlist.NET.Tools.Tests.TestUtilities;
+
+namespace Sqlist.NET.Tools.Tests.Integration;
+public class DependencyRegistrationTests
+{
+    [Fact]
+    public async Task TargetSide_DependencyInjection_WorksCorrectly()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        var appLifetimeMock = new Mock<IHostApplicationLifetime>();
+
+        services.AddLogging();
+        services.AddSingleton(appLifetimeMock.Object);
+        services.AddSqlist().AddSqlistTools();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var hostedServices = serviceProvider.GetServices<IHostedService>();
+
+        // Assert
+        try
+        {
+            foreach (var hostedService in hostedServices)
+            {
+                await hostedService.StartAsync(CancellationToken.None);
+            }
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail($"Expected no exception, but got: {ex}");
+        }
+    }
+
+    [Fact]
+    public async Task DotNetTool_DependencyInjection_WorksCorrectly()
+    {
+        // Arrange
+        var host = new HostBuilderMock()
+            .ConfigureServices(services => services.AddLogging())
+            .UseCommandLineApplication()
+            .Build();
+
+        // Assert
+        try
+        {
+            await host.StartAsync(CancellationToken.None);
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail($"Expected no exception, but got: {ex}");
+        }
+    }
+}
---
+++ test/Sqlist.NET.Tools.Test/TestUtilities/HostBuilderMock.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Moq;
+
+namespace Sqlist.NET.Tools.Tests.TestUtilities;
+internal class HostBuilderMock : IHostBuilder
+{
+    private readonly ServiceCollection _services = new();
+
+    public IDictionary<object, object> Properties => throw new NotImplementedException();
+
+    public IHost Build()
+    {
+        var hostMock = new Mock<IHost>();
+        var appLifetimeMock = new Mock<IHostApplicationLifetime>();
+
+        _services.AddSingleton(appLifetimeMock.Object);
+
+        var serviceProvider = _services.BuildServiceProvider();
+
+        hostMock.Setup(h => h.Services).Returns(serviceProvider);
+        hostMock.Setup(h => h.StartAsync(It.IsAny<CancellationToken>()))
+            .Returns(async (CancellationToken cancellationToken) =>
+            {
+                var services = serviceProvider.GetServices<IHostedService>();
+
+                foreach (var service in services)
+                {
+                    await service.StartAsync(cancellationToken);
+                }
+            });
+
+        return hostMock.Object;
+    }
+
+    public IHostBuilder ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureDelegate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IHostBuilder ConfigureContainer<TContainerBuilder>(Action<HostBuilderContext, TContainerBuilder> configureDelegate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IHostBuilder ConfigureHostConfiguration(Action<IConfigurationBuilder> configureDelegate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IHostBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureDelegate)
+    {
+        configureDelegate(null!, _services);
+        return this;
+    }
+
+    public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory) where TContainerBuilder : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(Func<HostBuilderContext, IServiceProviderFactory<TContainerBuilder>> factory) where TContainerBuilder : notnull
+    {
+        throw new NotImplementedException();
+    }
+}
