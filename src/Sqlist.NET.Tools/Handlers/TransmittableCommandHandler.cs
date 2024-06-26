95b12ea11eae50c75c8070f5b927988fe33febbb
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-06-26 19:18:26 +0300

    Removed the "required" constraint from the --project option, for that constraint doesn't fit the transmitted commands.

--- src/Sqlist.NET.Tools/Handlers/TransmittableCommandHandler.cs
+++ src/Sqlist.NET.Tools/Handlers/TransmittableCommandHandler.cs
@@ -16,7 +16,7 @@ internal abstract class TransmittableCommandHandler : ICommandHandler
 
     internal void Initialize(CommandLineApplication command)
     {
-        Project = command.Option("-p|--project <PROJECT>", Resources.ProjectDescription, CommandOptionType.SingleValue, o => o.IsRequired());
+        Project = command.Option("-p|--project <PROJECT>", Resources.ProjectDescription, CommandOptionType.SingleValue);
         Framework = command.Option("-f|--framework <FRAMEWORK>", Resources.FrameworkDescription, CommandOptionType.SingleValue);
         Configuration = command.Option("-c|--configuration <CONFIGURATION>", Resources.ConfigurationDescription, CommandOptionType.SingleValue);
         Runtime = command.Option("-r|--runtime <RUNTIME_IDENTIFIER>", Resources.RuntimeDescription, CommandOptionType.SingleValue);
