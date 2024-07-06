4a1bcd7777520a678d58915c7d1ce9b9be45058c
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2024-07-06 21:57:36 +0300

    Made the configureOptions parameter optional and changed the name of the method.

--- src/Sqlist.NET.Migration/Extensions/SqlistBuilderExtensions.cs
+++ src/Sqlist.NET.Migration/Extensions/SqlistBuilderExtensions.cs
@@ -6,28 +6,26 @@ using Sqlist.NET.Migration.Infrastructure;
 
 using System;
 
-namespace Sqlist.NET.Migration.Extensions
+namespace Sqlist.NET.Migration.Extensions;
+public static class SqlistBuilderExtensions
 {
-    public static class SqlistBuilderExtensions
+    /// <summary>
+    ///     Adds the services and configurations of the Sqlist.NET data migration framework.
+    /// </summary>
+    /// <param name="builder">The <see cref="SqlistBuilder"/> to configure.</param>
+    /// <param name="configureOptions">The configuration action to set up the Sqlist migration options.</param>
+    /// <returns>The <see cref="SqlistBuilder"/>.</returns>
+    public static SqlistBuilder WithMigration(this SqlistBuilder builder, Action<MigrationOptionsBuilder>? configureOptions = null)
     {
-        /// <summary>
-        ///     Adds the services and configurations of the Sqlist.NET data migration framework.
-        /// </summary>
-        /// <param name="builder">The <see cref="SqlistBuilder"/> to configure.</param>
-        /// <param name="configureOptions">The configuration action to set up the Sqlist migration options.</param>
-        /// <returns>The <see cref="SqlistBuilder"/>.</returns>
-        public static SqlistBuilder AddSqlistMigration(this SqlistBuilder builder, Action<MigrationOptionsBuilder> configureOptions)
+        builder.Services.Configure<MigrationOptions>(options =>
         {
-            builder.Services.Configure<MigrationOptions>(options =>
-            {
-                var builder = new MigrationOptionsBuilder(options);
-                configureOptions(builder);
-            });
+            var builder = new MigrationOptionsBuilder(options);
+            configureOptions?.Invoke(builder);
+        });
 
-            builder.Services.TryAddScoped<IMigrationService, MigrationService>();
-            builder.Services.TryAddScoped<IMigrationContext, MigrationContext>();
+        builder.Services.TryAddScoped<IMigrationService, MigrationService>();
+        builder.Services.TryAddScoped<IMigrationContext, MigrationContext>();
 
-            return builder;
-        }
+        return builder;
     }
 }
